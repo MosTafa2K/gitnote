3ced9a1799a89586133e5555d5d6aed2b903fdcd
mostafa2k <m2kappswindows8.1@gmail.com>
2024-12-01 18:49:12 +0330

    feat(config): add initialization for config file
    - Modifies `load_config` to initialize the config file if it doesn't exist, ensuring the application can always load a valid configuration.

--- gitnote/config/settings.py
+++ gitnote/config/settings.py
@@ -5,10 +5,19 @@ from pathlib import Path
 CONFIG_DIR = Path(__file__).resolve().parent.parent.parent / "config.yaml"
 
 
+def init_config():
+    if not Path(CONFIG_DIR).exists():
+        with open(CONFIG_DIR, "w") as f:
+            yaml.dump({"hf": {"token": ""}}, f)
+
+
 def load_config():
     if Path(CONFIG_DIR).exists():
         with open(CONFIG_DIR, "r") as f:
             return yaml.safe_load(f)
+    else:
+        init_config()
+        return {"hf": {"token": ""}}
 
 
 def save_config(config):
