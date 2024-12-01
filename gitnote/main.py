d8a77291a17aeb8274b02c4f5bef40a02fd85d8d
mostafa2k <m2kappswindows8.1@gmail.com>
2024-12-01 22:32:34 +0330

    feat: Add `set-token` command to set HuggingFace token
    - Adds a new command `set-token` to allow users to set their HuggingFace token. The token can be provided as an argument or prompted for input.

--- gitnote/main.py
+++ gitnote/main.py
@@ -1,11 +1,12 @@
 import time
+from typing import Annotated
 import typer
 from rich.progress import Progress, SpinnerColumn, TextColumn
 from rich.console import Console
 from gitnote.gittools.tools import get_git_diff, display_diff
 from gitnote.commitgen.generator import commit_generator
 from gitnote.utils import validate_token
-from gitnote.config.settings import load_token
+from gitnote.config.settings import load_token, save_token
 
 
 app = typer.Typer()
@@ -36,6 +37,20 @@ def generate():
 @app.command(name="diff", help="Display staged changes")
 def diff():
     display_diff()
+
+
+@app.command(name="set-token", help="Set token")
+def set_token(token: Annotated[str, typer.Argument()] = ""):
+    """Set HuggingFace Token
+
+    Args:
+        token (str, optional): A huggingface valid token. Defaults to "".
+    """
+    if token:
+        save_token(token)
+    else:
+        token = typer.prompt("Enter Token: ", hide_input=True)
+        save_token(token)
 
 
 @app.callback()
