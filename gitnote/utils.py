50a1e21727d69b1baabc6a429722afe567275f5e
mostafa2k <m2kappswindows8.1@gmail.com>
2024-12-01 12:21:48 +0330

    feat: Add token validation for Hugging Face API integration
    - Implemented `validate_token` function to verify the validity of the Hugging Face API token.
    - Added error handling for invalid tokens (`BadRequestError`, `LocalTokenNotFoundError`) and connection issues (`ConnectionError`, `ConnectTimeout`).
    - Provides user-friendly error messages for invalid token or connection issues.

---
+++ gitnote/utils.py
@@ -0,0 +1,28 @@
+from huggingface_hub import HfApi
+from huggingface_hub.errors import (
+    BadRequestError,
+    LocalTokenNotFoundError,
+)
+from requests.exceptions import ConnectionError, ConnectTimeout
+
+
+def validate_token(token: str) -> bool:
+    api = HfApi()
+    try:
+        api.whoami(token)
+        return True
+    except (
+        BadRequestError,
+        LocalTokenNotFoundError,
+    ):
+        print(
+            "Token is required or provided token is invalid!\n\
+Please provide a valid token using `gitnote setapikey <token>` command."
+        )
+        return False
+    except (
+        ConnectionError,
+        ConnectTimeout,
+    ):
+        print("Connection Error! Please check your internet connection and try again.")
+        return False
